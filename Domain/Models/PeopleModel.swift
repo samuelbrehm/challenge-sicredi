d0e131c401bb515f6d98349e0e86b04582ba5bc0
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-01 14:18:31 -0300

    Add PeopleModel

---
+++ Domain/Models/PeopleModel.swift
@@ -0,0 +1,15 @@
+//
+//  PeopleModel.swift
+//  Domain
+//
+//  Created by Samuel Brehm on 01/09/21.
+//
+
+import Foundation
+
+struct PeopleModel {
+    var id: String
+    var eventId: String
+    var name: String
+    var email: String
+}
