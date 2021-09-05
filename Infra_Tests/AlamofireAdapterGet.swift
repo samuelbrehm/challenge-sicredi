c63284b8a082350d8d40763440a5a6e7ea0856e2
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-05 17:07:32 -0300

    Ensure AlamofireAdapterGet calls with correct url and get method

--- Infra_Tests/AlamofireAdapterGet.swift
+++ Infra_Tests/AlamofireAdapterGet.swift
@@ -6,9 +6,59 @@
 //
 
 import XCTest
+import Alamofire
 
-class AlamofireAdapterGet: XCTestCase {
+class AlamofireAdapterGet {
+    private let session: Session
+    
+    public init(session: Session) {
+        self.session = session
+    }
+    
+    func get(to url: URL) {
+        session.request(url, method: .get).resume()
+    }
+}
 
-    func test_() throws {
+class AlamofireAdapterGetTests: XCTestCase {
+    
+    func test_get_should_make_call_request_with_correct_url_and_method() throws {
+        let url = makeURL()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
+        let sut = AlamofireAdapterGet(session: session)
+        sut.get(to: url)
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.observeRequest { request in
+            XCTAssertEqual(url, request.url)
+            XCTAssertEqual("GET", request.httpMethod)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+}
+
+class UrlProtocolStub: URLProtocol {
+    static var emit: ((URLRequest) -> Void)?
+    
+    static func observeRequest(completion: @escaping (URLRequest) -> Void) {
+        UrlProtocolStub.emit = completion
+    }
+    
+    open override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    open override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    open override func startLoading() {
+        UrlProtocolStub.emit?(request)
+    }
+
+    open override func stopLoading() {
+        
     }
 }
