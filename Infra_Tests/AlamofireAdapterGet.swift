ca1dd22a93e769a47e74dd7e91770daea48f71b9
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-05 17:01:08 -0300

    Add Alamofire dependence

---
+++ Infra_Tests/AlamofireAdapterGet.swift
@@ -0,0 +1,14 @@
+//
+//  Infra_Tests.swift
+//  Infra_Tests
+//
+//  Created by Samuel Brehm on 05/09/21.
+//
+
+import XCTest
+
+class AlamofireAdapterGet: XCTestCase {
+
+    func test_() throws {
+    }
+}
