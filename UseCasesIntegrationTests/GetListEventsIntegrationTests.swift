a763797747ff08f7eb1d84f88ceb730c895fff12
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-08 18:35:42 -0300

    add layer use case integration tests

---
+++ UseCasesIntegrationTests/GetListEventsIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  UseCasesIntegrationTests.swift
+//  UseCasesIntegrationTests
+//
+//  Created by Samuel Brehm on 08/09/21.
+//
+
+import XCTest
+
+class GetListEventsIntegrationTests: XCTestCase {
+    func test_() throws {
+    }
+}
