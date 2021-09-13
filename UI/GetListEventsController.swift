94a931cca940dda0a06bc04f3a701b665221a79f
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-13 19:21:47 -0300

    Ensure loading indicator has been hidden default

---
+++ UI/GetListEventsController.swift
@@ -0,0 +1,17 @@
+//
+//  GetListEventsController.swift
+//  UI
+//
+//  Created by Samuel Brehm on 13/09/21.
+//
+
+import Foundation
+import UIKit
+
+final class GetListEventsController: UIViewController {
+    @IBOutlet weak var loadingActivityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ UITests/GetListEventsControllerTests.swift
@@ -0,0 +1,20 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Samuel Brehm on 13/09/21.
+//
+
+import XCTest
+import Presentation
+import UIKit
+@testable import UI
+
+class GetListEventsControllerTests: XCTestCase {
+    func test_loading_is_hidden_on_start() throws {
+        let sb = UIStoryboard(name: "GetListEvents", bundle: Bundle(for: GetListEventsController.self))
+        let sut = sb.instantiateViewController(identifier: "GetListEventsController") as! GetListEventsController
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(sut.loadingActivityIndicator.isAnimating, false)
+    }
+}
