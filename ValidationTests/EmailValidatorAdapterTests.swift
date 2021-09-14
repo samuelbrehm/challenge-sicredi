e1a636d20cdf7da17b837e9a7406c1889956ed26
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-14 10:51:27 -0300

    Add Validation layer

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,13 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Samuel Brehm on 14/09/21.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+    func test_() throws {
+    }
+}
