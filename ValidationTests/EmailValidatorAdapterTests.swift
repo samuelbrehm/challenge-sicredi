d5e830ab15a86c591f98d1a7f4248d7fdf9881bc
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-14 11:02:57 -0300

    Create a single method for sut

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -20,11 +20,25 @@ public class EmailValidatorAdapter: EmailValidator {
 
 class EmailValidatorAdapterTests: XCTestCase {
     func test_invalid_emails() throws {
-        let sut = EmailValidatorAdapter()
+        let sut = makeSut()
         XCTAssertFalse(sut.isValid(email: "ss"))
         XCTAssertFalse(sut.isValid(email: "ss@"))
         XCTAssertFalse(sut.isValid(email: "ss@ss"))
         XCTAssertFalse(sut.isValid(email: "ss@ss."))
         XCTAssertFalse(sut.isValid(email: "@ss.com"))
+    }
+    
+    func test_valid_emails() throws {
+        let sut = makeSut()
+        XCTAssertTrue(sut.isValid(email: "any-name@gmail.com"))
+        XCTAssertTrue(sut.isValid(email: "any-name@email.com"))
+        XCTAssertTrue(sut.isValid(email: "any-email@outlook.com"))
+        XCTAssertTrue(sut.isValid(email: "any-email@outlook.com.br"))
+    }
+}
+
+extension EmailValidatorAdapterTests {
+    func makeSut() -> EmailValidatorAdapter {
+        return EmailValidatorAdapter()
     }
 }
