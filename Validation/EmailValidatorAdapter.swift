8ecb41a0ef75c604a8ec3a21ac05a53a213be51a
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-14 11:05:01 -0300

    Create EmailValidatorAdapter into Validation layer

---
+++ Validation/EmailValidatorAdapter.swift
@@ -0,0 +1,21 @@
+//
+//  EmailValidatorAdapter.swift
+//  Validation
+//
+//  Created by Samuel Brehm on 14/09/21.
+//
+
+import Foundation
+import Presentation
+
+public class EmailValidatorAdapter: EmailValidator {
+    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    public init() {}
+    
+    public func isValid(email: String) -> Bool {
+        let range = NSRange(location: 0, length: email.utf16.count)
+        let regex = try! NSRegularExpression(pattern: pattern)
+        return regex.firstMatch(in: email, options: [], range: range) != nil
+    }
+}
