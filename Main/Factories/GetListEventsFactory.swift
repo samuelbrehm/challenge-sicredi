9fee30a0e0e3106ab57607c843262f61245f17ba
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-17 16:32:53 -0300

    Refactor to single file class WeakVarProxy

--- Main/Factories/GetListEventsFactory.swift
+++ Main/Factories/GetListEventsFactory.swift
@@ -8,9 +8,6 @@
 import Foundation
 import UI
 import Presentation
-import Data
-import Infra_
-import Domain
 
 class GetListEventsFactory {
     static func makeController() -> GetListEventsViewController {
@@ -20,33 +17,4 @@ class GetListEventsFactory {
         
         return controller
     }
-}
-
-class WeakVarProxy<T: AnyObject> {
-    private weak var instance: T?
-    
-    init(_ instance: T) {
-        self.instance = instance
-    }
-}
-
-extension WeakVarProxy: AlertView where T: AlertView {
-    func showMessage(viewModel: AlertViewModel) {
-        instance?.showMessage(viewModel: viewModel)
-    }
-}
-
-
-extension WeakVarProxy: LoadingView where T: LoadingView {
-    func display(viewModel: LoadingViewModel) {
-        instance?.display(viewModel: viewModel)
-    }
-}
-
-extension WeakVarProxy: EventsView where T: EventsView {
-    func showEvents(viewModel: [EventsViewModel]) {
-        instance?.showEvents(viewModel: viewModel)
-    }
-    
-   
 }
---
+++ Main/Proxies/WeakVarProxy.swift
@@ -0,0 +1,36 @@
+//
+//  WeakVarProxy.swift
+//  Main
+//
+//  Created by Samuel Brehm on 17/09/21.
+//
+
+import Foundation
+import Presentation
+
+class WeakVarProxy<T: AnyObject> {
+    private weak var instance: T?
+    
+    init(_ instance: T) {
+        self.instance = instance
+    }
+}
+
+extension WeakVarProxy: AlertView where T: AlertView {
+    func showMessage(viewModel: AlertViewModel) {
+        instance?.showMessage(viewModel: viewModel)
+    }
+}
+
+
+extension WeakVarProxy: LoadingView where T: LoadingView {
+    func display(viewModel: LoadingViewModel) {
+        instance?.display(viewModel: viewModel)
+    }
+}
+
+extension WeakVarProxy: EventsView where T: EventsView {
+    func showEvents(viewModel: [EventsViewModel]) {
+        instance?.showEvents(viewModel: viewModel)
+    }
+}
