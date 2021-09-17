80aa460320f3a794655c2afa1f3e77845b3681b8
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-17 16:29:14 -0300

    Create class to implement Proxy Pattern to avoid memory leak into view controller

--- Main/Factories/GetListEventsFactory.swift
+++ Main/Factories/GetListEventsFactory.swift
@@ -15,9 +15,38 @@ import Domain
 class GetListEventsFactory {
     static func makeController() -> GetListEventsViewController {
         let controller =  GetListEventsViewController.instantiate()
-        let presenter = ListEventsPresenter(getListEvents: makeGetListEvents(), alertView: controller, loadingView: controller, eventsView: controller)
+        let presenter = ListEventsPresenter(getListEvents: makeGetListEvents(), alertView: WeakVarProxy(controller), loadingView: WeakVarProxy(controller), eventsView: WeakVarProxy(controller))
         controller.loadListEvents = presenter.showEventsList
         
         return controller
     }
+}
+
+class WeakVarProxy<T: AnyObject> {
+    private weak var instance: T?
+    
+    init(_ instance: T) {
+        self.instance = instance
+    }
+}
+
+extension WeakVarProxy: AlertView where T: AlertView {
+    func showMessage(viewModel: AlertViewModel) {
+        instance?.showMessage(viewModel: viewModel)
+    }
+}
+
+
+extension WeakVarProxy: LoadingView where T: LoadingView {
+    func display(viewModel: LoadingViewModel) {
+        instance?.display(viewModel: viewModel)
+    }
+}
+
+extension WeakVarProxy: EventsView where T: EventsView {
+    func showEvents(viewModel: [EventsViewModel]) {
+        instance?.showEvents(viewModel: viewModel)
+    }
+    
+   
 }
