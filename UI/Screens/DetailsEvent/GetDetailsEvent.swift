09b120cd5764a1a32b860faca063d951673e8ed4
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-19 11:10:41 -0300

    Build layout storyboard with constraints

---
+++ UI/Screens/DetailsEvent/GetDetailsEvent.swift
@@ -0,0 +1,13 @@
+//
+//  GetDetailsEvent.swift
+//  UI
+//
+//  Created by Samuel Brehm on 19/09/21.
+//
+
+import Foundation
+
+import UIKit
+import Presentation
+
+public final class GetDetailsEventViewController: UIViewController, Storyboarded {}
