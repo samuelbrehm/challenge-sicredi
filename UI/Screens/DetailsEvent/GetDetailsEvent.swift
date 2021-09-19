ff9106680941b831db34b430811df51fe515060c
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-19 18:04:21 -0300

    Create initial setup ViewController of the UI Details Event

--- UI/Screens/DetailsEvent/GetDetailsEvent.swift
+++ UI/Screens/DetailsEvent/GetDetailsEvent.swift
@@ -8,6 +8,32 @@
 import Foundation
 
 import UIKit
+import MapKit
 import Presentation
 
-public final class GetDetailsEventViewController: UIViewController, Storyboarded {}
+public final class GetDetailsEventViewController: UIViewController, Storyboarded {
+    
+    @IBOutlet weak var eventImageView: UIImageView!
+    @IBOutlet weak var titleEventLabel: UILabel!
+    @IBOutlet weak var dayEventLabel: UILabel!
+    @IBOutlet weak var monthEventLabel: UILabel!
+    @IBOutlet weak var wrapperContetDateView: UIView!
+    @IBOutlet weak var descriptionEventLabel: UILabel!
+    @IBOutlet weak var locationEventMap: MKMapView!
+    @IBOutlet weak var loadingActivityIndicator: UIActivityIndicatorView!
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureUI()
+    }
+    
+    private func configureUI() {
+        self.loadingActivityIndicator?.layer.cornerRadius = 9
+        
+        self.wrapperContetDateView.layer.shadowColor = UIColor(red: 52, green: 199, blue: 89, alpha: 0.2).cgColor
+        self.wrapperContetDateView.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        self.wrapperContetDateView.layer.shadowOpacity = 1.0
+        self.wrapperContetDateView.layer.masksToBounds = false
+        self.wrapperContetDateView.layer.cornerRadius = 6.0
+    }
+}
