73183e4b3d6474278d1429f41c3dd9ef2e6809d4
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-21 11:17:13 -0300

    create file colors with primary color

---
+++ UI/Helpers/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  UI
+//
+//  Created by Samuel Brehm on 21/09/21.
+//
+
+import Foundation
+import UIKit
+
+struct Color {
+    static let primary = UIColor(hex: "#64E291")
+}
