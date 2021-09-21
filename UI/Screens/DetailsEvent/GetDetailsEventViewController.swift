11bed098dca633d39ac78364bb074e8db63fd283
Samuel Brehm <samuelbrehm@gmail.com>
2021-09-20 23:13:25 -0300

    Add scrollview layout

--- UI/Screens/DetailsEvent/GetDetailsEventViewController.swift
+++ UI/Screens/DetailsEvent/GetDetailsEventViewController.swift
@@ -16,9 +16,7 @@ public final class GetDetailsEventViewController: UIViewController, Storyboarded
     
     @IBOutlet weak var eventImageView: UIImageView!
     @IBOutlet weak var titleEventLabel: UILabel!
-    @IBOutlet weak var dayEventLabel: UILabel!
-    @IBOutlet weak var monthEventLabel: UILabel!
-    @IBOutlet weak var wrapperContetDateView: UIView!
+    @IBOutlet weak var dateEventLabel: UILabel!
     @IBOutlet weak var descriptionEventLabel: UILabel!
     @IBOutlet weak var locationEventMap: MKMapView!
     @IBOutlet weak var loadingActivityIndicator: UIActivityIndicatorView!
@@ -41,12 +39,6 @@ public final class GetDetailsEventViewController: UIViewController, Storyboarded
     
     private func configureUI() {
         self.loadingActivityIndicator?.layer.cornerRadius = 9
-        
-        self.wrapperContetDateView.layer.shadowColor = UIColor(red: 52, green: 199, blue: 89, alpha: 0.2).cgColor
-        self.wrapperContetDateView.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        self.wrapperContetDateView.layer.shadowOpacity = 1.0
-        self.wrapperContetDateView.layer.masksToBounds = false
-        self.wrapperContetDateView.layer.cornerRadius = 6.0
     }
     
     private func configureEventViewModel() {
@@ -67,7 +59,7 @@ public final class GetDetailsEventViewController: UIViewController, Storyboarded
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "dd MMMM - HH:mm 'h'"
             let dateEventFormatted  = dateFormatter.string(from: date)
-            self.monthEventLabel.text = dateEventFormatted
+            self.dateEventLabel.text = dateEventFormatted
         }
     }
 }
